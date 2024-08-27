f428fcc14db82fca95da27addc534923a6d5928c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-27 20:12:29 +0300

    loops in control flow

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+   count_down = 10
+   while count_down > 0:
+       print(count_down)
+       count_down -= 1
+   print('Happy New Year!')     
+# happy_new_year()      
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers = [ints ** 2 for ints in int_list]
+    return squared_integers
+
+# print(square_integers([2, 3, 4, 5, 6]))
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   for num in range(1, 101):
+       if (num % 3 == 0 and num % 5 == 0):
+           print('FizzBuzz')
+       elif num % 3 == 0:
+           print('Fizz')
+       elif num % 5 == 0:
+           print('Buzz')
+       else:
+           print(num)
+fizzbuzz()                   
